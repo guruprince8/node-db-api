6db51e694437db04839f2e3162c00a2acbb6eb12
guruprince8 <guru.prince8@gmail.com>
2023-02-01 11:57:54 +0530

    snchronous messages

--- index.js
+++ index.js
@@ -1,16 +1,57 @@
+//require('dotenv').config();
+// https://github.com/oracle/node-oracledb/blob/main/examples/example.js
+// https://www.oracle.com/webfolder/technetwork/tutorials/obe/cloud/apaas/node/node-employee-service/node-employee-service.html
 const oracledb = require('oracledb');
 const dbconfig = require('./dbconfig');
+const express = require('express');
+const app = express();
+const port = 3000;
 let connection;
+//console.log(process.env);
+// console.log(process.platform + ' ' + process.env.HOME + " " + process.env.PORT + " " + process.env);
 try {
-  oracledb.initOracleClient({libDir: 'D:\\guru\\node\\instantclient_19_17'});
-  console.log(oracledb);
+    oracledb.initOracleClient({ libDir: 'D:\\guru\\node\\instantclient_19_17' });
+    //console.log(oracledb);
 } catch (err) {
-  console.error('Whoops!');
-  console.error(err);
-  process.exit(1);
+    console.error('Whoops!');
+    console.error(err);
+    process.exit(1);
 }
-async function getDBConnection() {
-    connection = await oracledb.getConnection(dbconfig);
-    console.log(connection);
+function getDBConnection() {
+    ;
+    //console.log("After connection"+connection);
+    //console.log(connection);
 }
-getDBConnection();
+// getDBConnection();
+app.get('/', (req, res) => {
+    oracledb.getConnection(dbconfig, function (err, connection) {
+        console.log(connection);
+        if (err) {
+            console.error(err.message);
+            response.status(500).send("Error connecting to DB");
+            return;
+        } else {
+            let sql = `SELECT * FROM system.sample`;
+            let bind = {};
+            let options = {
+                outFormat: oracledb.OUT_FORMAT_OBJECT,
+            };
+            let result;
+
+            result = connection.execute(sql, bind, options,function(err,result){
+                if(err){
+                    console.error(err.message);
+                    response.status(500).send("Error getting data from DB");
+                } else {
+                    res.send(JSON.stringify(result));
+                }
+            });
+
+            
+        }
+    });
+
+});
+app.listen(port, () => {
+    console.log(`listening on port ${port}`);
+});
