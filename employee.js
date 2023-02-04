1a353e3398f21b1f86f3fd591edbf9921bef53fb
guruprince8 <guru.prince8@gmail.com>
2023-02-04 17:42:38 +0530

    node standards

--- employee.js
+++ employee.js
@@ -1,19 +1,27 @@
 // ref link - https://www.oracle.com/webfolder/technetwork/tutorials/obe/cloud/apaas/node/node-employee-service/node-employee-service.html
-// express routers
+// express , oracle db and uuid imports
 const express = require('express');
+const oracledb = require('oracledb');
+const { v4: uuidv4 } = require('uuid');
+// emp router
 const app = express();
 const empservices = express.Router();
-// oracle db configurations
-const oracledb = require('oracledb');
 
-// emp services request handler
+// emp services starting request handler
 empservices.use((req, res, next) => {
-    console.log('Time: ' + new Date() + " employee router");
+    console.log("REQUEST:" + req.method + "   " + req.url);
+    console.log("BODY:" + JSON.stringify(req.body));
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+    res.setHeader('Access-Control-Allow-Credentials', true);
+    res.setHeader("x-service-trace-id", uuidv4());
+    res.setHeader("Content-Type","application/json");                         
     next();
 });
 
 // fetch all employees
-empservices.get('/all', (req, res) => {
+empservices.get('/v1/employee/all', (req, res) => {
     console.log(new Date() + " employee services - method " + req.url);
     oracledb.getConnection("hr", (err, connection) => {
         if (!err) {
@@ -22,27 +30,26 @@ empservices.get('/all', (req, res) => {
                     console.error(err.message);
                     response.status(500).send("Error getting data from DB");
                 } else {
-                    console.log(new Date() + " employee services - method -response " + JSON.stringify(result));
+                    //console.log(new Date() + " employee services - method -response " + JSON.stringify(result));
                     let employees = [];
-                    result.rows.forEach((employee)=>{
+                    result.rows.forEach((employee) => {
                         employees.push(
                             {
-                              id:employee.id,
-                              firstName:employee.FIRSTNAME,
-                              lastName:employee.LASTNAME,
-                              email:employee.EMAIL,
-                              phone:employee.PHONE,
-                              bithDate:employee.BIRTHDATE,
-                              title:employee.TITLE,
-                              department:employee.DEPARTMENT   
+                                id: employee.id,
+                                firstName: employee.FIRSTNAME,
+                                lastName: employee.LASTNAME,
+                                email: employee.EMAIL,
+                                phone: employee.PHONE,
+                                bithDate: employee.BIRTHDATE,
+                                title: employee.TITLE,
+                                department: employee.DEPARTMENT
                             }
                         )
                     });
-                    res.setHeader("Content-Type","application/json");
-                    let empresponse = {
-                        "employees":employees
+                    let empAllResponse = {
+                        "employees": employees
                     };
-                    res.status(200).send(empresponse);
+                    res.status(200).send(empAllResponse);
                 }
             });
 
@@ -52,4 +59,60 @@ empservices.get('/all', (req, res) => {
     });
 });
 
+// fetch employee based on emp id 
+empservices.get("/v1/employee/:empId", (req, res) => {
+    console.log(new Date() + " employee services - method " + req.url);
+    oracledb.getConnection("hr", (err, connection) => {
+        if (!err) {
+            connection.execute("SELECT * FROM system.employee where id=:empId and rownum<=1", [req.params.empId], { outFormat: oracledb.OBJECT }, (err, result) => {
+                if (err) {
+                    console.error(err.message);
+                    response.status(500).send("Error getting data from DB");
+                } else {
+                    //console.log(new Date() + " employee services - method -response " + JSON.stringify(result));
+                    let employee = {};
+                    result.rows.forEach((emp) => {
+                        employee.id = emp.ID;
+                        employee.firstName = emp.FIRSTNAME;
+                        employee.lastName = emp.LASTNAME;
+                        employee.email = emp.EMAIL;
+                        employee.phone = emp.PHONE;
+                        employee.bithDate = emp.BIRTHDATE;
+                        employee.title = emp.TITLE;
+                        employee.department = emp.DEPARTMENT;
+
+                    });
+                    res.status(200).send(employee);
+                }
+            });
+
+        } else {
+            response.status(500).send("Error connecting to DB");
+        }
+    });
+});
+
+// add new employee 
+empservices.post("/v1/employee", (req, res) => {
+    oracledb.getConnection("hr", (err, connection) => {
+        if (!err) {
+            connection.execute("INSERT INTO EMPLOYEE (ID, FIRSTNAME, LASTNAME, EMAIL, PHONE, BIRTHDATE, TITLE, DEPARTMENT)" +
+                "VALUES(EMPLOYEE_SEQ.NEXTVAL, :firstName,:lastName,:email,:phone,:birthdate,:title,:department)",
+                [req.body.firstName, req.body.lastName, req.body.email, req.body.phone, req.body.bithDate, req.body.title, req.body.department],
+                { outFormat: oracledb.OBJECT,autoCommit: true },
+                (err, result) => {
+                    if (err) {
+                        console.error(err.message);
+                        response.status(500).send("Error getting data from DB");
+                    } else {
+                        res.status(200).send(JSON.stringify(result));
+                    }
+                });
+
+        } else {
+            response.status(500).send("Error connecting to DB");
+        }
+    });
+
+});
 module.exports = empservices;
--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-//require('dotenv').config();
+// require('dotenv').config();
 // https://github.com/oracle/node-oracledb/blob/main/examples/example.js
 // https://www.oracle.com/webfolder/technetwork/tutorials/obe/cloud/apaas/node/node-employee-service/node-employee-service.html
 // https://www.oracle.com/in/database/technologies/instant-client/linux-x86-64-downloads.html
@@ -6,12 +6,13 @@
 // https://node-oracledb.readthedocs.io/en/latest/api_manual/pool.html
 // https://github.com/oracle/node-oracledb/blob/main/examples/webapp.js
 const express = require('express');
-const app = express();
-const port = 3000;
-
 const dbconfig = require('./dbconfig');
 const oracledb = require('oracledb');
 const initOracleClient = require('./initoraclient');
+
+const app = express();
+app.use(express.json());
+const port = 3000;
 initOracleClient();
 
 const empservices = require('./employee');
@@ -19,7 +20,7 @@ const empservices = require('./employee');
 async function init() {
     try {
         await oracledb.createPool(dbconfig);
-        app.use('/employee',empservices);
+        app.use('/employee-services',empservices);
         app.listen(port,()=> {
             console.log(`Example app listening on port ${port}`);
         })
